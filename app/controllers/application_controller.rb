a0f164d3800b504e3b74e7d82405affbb8d41248
Jerry Shi <jerryshi25@gmail.com>
2019-11-16 21:58:01 +0000

    work in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  def return_user_array
+    u = User.all
+    render({ :plain => u.to_json})
+  end
+
+  def return_user
+    u = User.where({ :username => params.fetch(:username)})
+    render({ :plain => u.to_json})
+  end
+
+  def return_photos
+    u = User.where({ :username => params.fetch(:username)}).first
+    u_photos = Photo.where({ :owner_id => u.id})
+    render({ :plain => u_photos.to_json})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  match("/users", { :controller => "application", :action => "return_user_array", :via => "get"})
+
+  match("/users/:username", { :controller => "application", :action => "return_user", :via => "get"})
+
+  match("/users/:username/own_photos", { :controller => "application", :action => "return_photos", :via => "get"})
+
 
   # =================================================
   devise_for :admin_users, ActiveAdmin::Devise.config
