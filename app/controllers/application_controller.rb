5a3b9431693b2cedbaf38bc436f308015df9a68b
Jerry Shi <jerryshi25@gmail.com>
2019-11-17 18:20:23 +0000

    finished photogram api assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,82 @@ class ApplicationController < ActionController::Base
   end
 
   def return_user
-    u = User.where({ :username => params.fetch(:username)})
+    u = User.where({ :username => params.fetch(:username)}).first
     render({ :plain => u.to_json})
   end
 
   def return_photos
     u = User.where({ :username => params.fetch(:username)}).first
-    u_photos = Photo.where({ :owner_id => u.id})
-    render({ :plain => u_photos.to_json})
+    render({ :plain => u.own_photos.to_json             })
+  end
+
+  def return_likes
+    u = User.where({ :username => params.fetch(:username)}).first
+    render({ :plain => u.liked_photos.to_json})
+  end
+
+  def return_feed
+    u = User.where({ :username => params.fetch(:username)}).first
+    render({ :plain => u.feed.to_json})
+  end
+  
+  def return_discover
+    u = User.where({ :username => params.fetch(:username)}).first
+    render({ :plain => u.discover.to_json})
+  end
+  
+  def return_photo
+    photo = Photo.where({ :id => params.fetch(:id)}).first
+    render({ :plain => photo.to_json})
+  end
+
+  def return_photo_likes
+    photo = Photo.where({ :id => params.fetch(:id)}).first
+    render({ :plain => photo.likes.to_json})
+  end
+
+  def return_photo_fans
+    photo = Photo.where({ :id => params.fetch(:id)}).first
+    render({ :plain => photo.fans.to_json})
+  end
+
+  def insert_like_record
+    l = Like.new
+    l.fan_id = params.fetch(:input_user_id).to_i
+    l.photo_id = params.fetch(:input_photo_id).to_i
+    l.created_at = Time.now
+    l.updated_at = Time.now
+    l.save
+    render({ :plain => l.to_json})
+  end
+
+  def delete_like
+    l = Like.where({ :id => params.fetch(:id)}).first
+    l.destroy
+    render({ :plain => l.to_json}) #returns deleted like entry
+  end
+
+  def return_photo_comments
+    photo = Photo.where({ :id => params.fetch(:id)}).first
+    render({ :plain => photo.comments.to_json})
+  end
+
+  def insert_comment_record
+    c = Comment.new
+    c.body = params.fetch(:input_body)
+    c.author_id = params.fetch(:input_user_id)
+    c.photo_id = params.fetch(:input_photo_id)
+    c.created_at = Time.now
+    c.updated_at = Time.now
+    c.save
+    render({ :plain => c.to_json})
+  end
+
+  def update_comment_record
+    c = Comment.where({ :id => params.fetch(:id)}).first
+    c.body = params.fetch(:input_body)
+    c.updated_at = Time.now
+    c.save
+    render({ :plain => c.to_json})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,27 @@ Rails.application.routes.draw do
 
   match("/users/:username/own_photos", { :controller => "application", :action => "return_photos", :via => "get"})
 
+  match("/users/:username/liked_photos", { :controller => "application", :action => "return_likes", :via => "get"})
+
+  match("/users/:username/feed", { :controller => "application", :action => "return_feed", :via => "get"})
+
+  match("/users/:username/discover", { :controller => "application", :action => "return_discover", :via => "get"})
+
+  match("/photos/:id", { :controller => "application", :action => "return_photo", :via => "get"})
+
+  match("/photos/:id/likes", { :controller => "application", :action => "return_photo_likes", :via => "get"})
+
+  match("/photos/:id/fans", { :controller => "application", :action => "return_photo_fans", :via => "get"})
+
+  match("/insert_like_record", { :controller => "application", :action => "insert_like_record", :via => "get"})
+
+  match("/delete_like/:id", { :controller => "application", :action => "delete_like", :via => "get"})
+
+  match("/photos/:id/comments", { :controller => "application", :action => "return_photo_comments", :via => "get"})
+
+  match("/insert_comment_record", { :controller => "application", :action => "insert_comment_record", :via => "get"})
+
+  match("/update_comment_record/:id", { :controller => "application", :action => "update_comment_record", :via => "get"})
 
   # =================================================
   devise_for :admin_users, ActiveAdmin::Devise.config
